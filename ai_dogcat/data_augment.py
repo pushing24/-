edba5f5d6878e34d656ae64781da52c09841e86a
pushing24 <3395013833@qq.com>
2025-04-19 22:42:55 +0800

    Add files via upload

---
+++ ai_dogcat/data_augment.py
@@ -0,0 +1,11 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+datagen = ImageDataGenerator(
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
